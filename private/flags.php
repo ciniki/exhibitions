b8c0b1330f50e46602b5dc14e533c4d153b8b027
Andrew Rivett <veggiefrog@gmail.com>
2014-06-18 17:07:56 -0400

    Added module flags

---
+++ private/flags.php
@@ -0,0 +1,22 @@
+<?php
+//
+// Description
+// -----------
+// The module flags
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_exhibitions_flags($ciniki, $modules) {
+	$flags = array(
+		array('flag'=>array('bit'=>'1', 'name'=>'Exhibitors')),
+		array('flag'=>array('bit'=>'2', 'name'=>'Sponsors')),
+		array('flag'=>array('bit'=>'3', 'name'=>'Tour')),
+		);
+
+	return array('stat'=>'ok', 'flags'=>$flags);
+}
+?>
